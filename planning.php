0324d0851742f3f2885f21acea37880fbe5ffcc5
[Mziguel] <[mziguel.nohaila@gmail.com]>
2020-03-22 00:09:27 +0100

    Modifier par Nohaila Mziguel

--- planning.php
+++ planning.php
@@ -15,7 +15,8 @@ body  {
     <!-- entete -->
     <?php include('entete.php'); ?>	
     <!-- Section -->	
-<section> 
+<section>
+<h2>Modifier Par Nohaila Mziguel</h2> 
     <article>	
             <HR color=#111>
             <p class="style-titre1" style="text-align:center;">Lundi 	</p>				
